5355b2d21791ccd46b74f8160f2bd6d821c7d95b
dmoldabayev <dmoldabayev.99@gmail.com>
2021-11-30 20:10:30 +0600

    made some changes

--- src/main/scala/services/PlayerService.scala
+++ src/main/scala/services/PlayerService.scala
@@ -22,74 +22,62 @@ class PlayerService(implicit val redis: Redis,
                     ex: ExecutionContext,
                     formats: Formats,
                     roomCol: MongoCollection[Room],
-                    userRoomCol: MongoCollection[RoomUser] ) {
+                    userRoomCol: MongoCollection[RoomUser]) {
 
   val roomRepository = new RoomRepository()
   val roomUserRepository = new RoomUserRepository()
 
   def controlPlayer(amqpMessage: AMQPMessage): Future[Unit] = {
-   val (tokenKey, roomId) = amqpMessage.routingKey match {
+    val (tokenKey, roomId) = amqpMessage.routingKey match {
       case RoomCore.PauseSong.routingKey => (parse(amqpMessage.entity).extract[PauseSongCommandBody].tokenKey, parse(amqpMessage.entity).extract[PauseSongCommandBody].roomId)
       case RoomCore.PlayPrevTrack.routingKey => (parse(amqpMessage.entity).extract[PrevSongCommandBody].tokenKey, parse(amqpMessage.entity).extract[PrevSongCommandBody].roomId)
-      case RoomCore.PlayNextTrack.routingKey => (parse(amqpMessage.entity).extract[NextSongCommandBody].tokenKey,parse(amqpMessage.entity).extract[NextSongCommandBody].roomId )
-      case RoomCore.PlaySong.routingKey => (parse(amqpMessage.entity).extract[PlaySongCommandBody].tokenKey,parse(amqpMessage.entity).extract[PlaySongCommandBody].roomId)
+      case RoomCore.PlayNextTrack.routingKey => (parse(amqpMessage.entity).extract[NextSongCommandBody].tokenKey, parse(amqpMessage.entity).extract[NextSongCommandBody].roomId)
+      case RoomCore.PlaySong.routingKey => (parse(amqpMessage.entity).extract[PlaySongCommandBody].tokenKey, parse(amqpMessage.entity).extract[PlaySongCommandBody].roomId)
     }
 
     (for {
       roomUsers <- roomUserRepository.findAllByFilter[RoomUser](equal("roomId", roomId))
       activeUsers <- Future(roomUsers.filter(_.`type` != RoomUserType.NOT_ACTIVE))
-      accessTokens <- Future.sequence(activeUsers.map( user => redis.get(user.id)))
     } yield {
-      accessTokens.foreach {
-        case Some(value) =>
-          amqpMessage.routingKey match {
-            case RoomCore.PauseSong.routingKey =>
-              val request = write(PlayerPauseGatewayCommandBody(deviceId = None, tokenKey = value))
-              publisher ! AMQPMessage(request, "X:mounty-spotify-gateway-in", SpotifyGateway.PlayerPauseGatewayCommand.routingKey, amqpMessage.actorPath)
+      activeUsers.foreach { user =>
+        amqpMessage.routingKey match {
+          case RoomCore.PauseSong.routingKey =>
+            val request = write(PlayerPauseGatewayCommandBody(deviceId = None, tokenKey = user.profileId))
+            publisher ! AMQPMessage(request, "X:mounty-spotify-gateway-in", SpotifyGateway.PlayerPauseGatewayCommand.routingKey, amqpMessage.actorPath)
 
-            case RoomCore.PlayPrevTrack.routingKey =>
-              val request = write(PlayerPrevGatewayCommandBody(deviceId = None, tokenKey = value))
-              publisher ! AMQPMessage(request, "X:mounty-spotify-gateway-in", SpotifyGateway.PlayerPrevGatewayCommand.routingKey, amqpMessage.actorPath)
+          case RoomCore.PlayPrevTrack.routingKey =>
+            val request = write(PlayerPrevGatewayCommandBody(deviceId = None, tokenKey = user.profileId))
+            publisher ! AMQPMessage(request, "X:mounty-spotify-gateway-in", SpotifyGateway.PlayerPrevGatewayCommand.routingKey, amqpMessage.actorPath)
 
-            case RoomCore.PlayNextTrack.routingKey =>
-              val request = write(PlayerNextGatewayCommandBody(deviceId = None, tokenKey = value))
-              publisher ! AMQPMessage(request, "X:mounty-spotify-gateway-in", SpotifyGateway.PlayerNextGatewayCommand.routingKey, amqpMessage.actorPath)
+          case RoomCore.PlayNextTrack.routingKey =>
+            val request = write(PlayerNextGatewayCommandBody(deviceId = None, tokenKey = user.profileId))
+            publisher ! AMQPMessage(request, "X:mounty-spotify-gateway-in", SpotifyGateway.PlayerNextGatewayCommand.routingKey, amqpMessage.actorPath)
 
-            case RoomCore.PlaySong.routingKey =>
-              val body = parse(amqpMessage.entity).extract[PlaySongCommandBody]
-              val request = write(PlayerPlayGatewayCommandBody(deviceId = None, tokenKey = value, contextUri = body.contextUri, offset = body.offset))
-              publisher ! AMQPMessage(request, "X:mounty-spotify-gateway-in", SpotifyGateway.PlayerPlayGatewayCommand.routingKey, amqpMessage.actorPath)
-
-          }
-        case None =>
-          val error = ServerErrorRequestException(
-            ErrorCodes.INTERNAL_SERVER_ERROR(ErrorSeries.ROOM_CORE),
-            Some("token not found")
-          ).getMessage
-          val reply = write(error)
-          publisher ! amqpMessage.copy(entity = reply, routingKey = MountyApi.Error.routingKey, exchange = "X:mounty-api-out")
+          case RoomCore.PlaySong.routingKey =>
+            val body = parse(amqpMessage.entity).extract[PlaySongCommandBody]
+            val request = write(PlayerPlayGatewayCommandBody(deviceId = None, tokenKey = user.profileId, contextUri = body.contextUri, offset = body.offset))
+            publisher ! AMQPMessage(request, "X:mounty-spotify-gateway-in", SpotifyGateway.PlayerPlayGatewayCommand.routingKey, amqpMessage.actorPath)
+        }
       }
       amqpMessage.routingKey match {
         case RoomCore.PauseSong.routingKey =>
-          publisher ! amqpMessage.copy(entity = write(PauseSongResponseBody()),routingKey = MountyApi.PauseSongResponse.routingKey, exchange =  "X:mounty-api-out")
+          publisher ! amqpMessage.copy(entity = write(PauseSongResponseBody()), routingKey = MountyApi.PauseSongResponse.routingKey, exchange = "X:mounty-api-out")
         case RoomCore.PlayPrevTrack.routingKey =>
-          publisher ! amqpMessage.copy( entity = write(PrevSongResponseBody()),routingKey = MountyApi.PlayPrevTrackResponse.routingKey, exchange =  "X:mounty-api-out")
+          publisher ! amqpMessage.copy(entity = write(PrevSongResponseBody()), routingKey = MountyApi.PlayPrevTrackResponse.routingKey, exchange = "X:mounty-api-out")
         case RoomCore.PlayNextTrack.routingKey =>
-          publisher ! amqpMessage.copy(entity = write(NextSongResponseBody()),routingKey = MountyApi.PlayNextTrackResponse.routingKey, exchange =  "X:mounty-api-out")
+          publisher ! amqpMessage.copy(entity = write(NextSongResponseBody()), routingKey = MountyApi.PlayNextTrackResponse.routingKey, exchange = "X:mounty-api-out")
         case RoomCore.PlaySong.routingKey =>
-          publisher ! amqpMessage.copy(entity = write(PlaySongResponseBody()),routingKey = MountyApi.PlaySongResponse.routingKey, exchange =  "X:mounty-api-out")
+          publisher ! amqpMessage.copy(entity = write(PlaySongResponseBody()), routingKey = MountyApi.PlaySongResponse.routingKey, exchange = "X:mounty-api-out")
       }
     }
-    ).recover {
+      ).recover {
       case e: Throwable =>
         val error = ServerErrorRequestException(
           ErrorCodes.INTERNAL_SERVER_ERROR(ErrorSeries.ROOM_CORE),
           Some(e.getMessage)
         ).getMessage
         val reply = write(error)
-        publisher ! amqpMessage.copy(entity = reply, routingKey = MountyApi.Error.routingKey, exchange =  "X:mounty-api-out")
+        publisher ! amqpMessage.copy(entity = reply, routingKey = MountyApi.Error.routingKey, exchange = "X:mounty-api-out")
     }
   }
-
-
 }
