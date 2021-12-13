1e25f11882de2a552c2a97d5705d475490ffb042
DMoldabaev <DMoldabaev@one.kz>
2021-12-13 22:56:26 +0600

    added logic for GetCurrentlyPlayingTrack

--- src/main/scala/services/PlayerService.scala
+++ src/main/scala/services/PlayerService.scala
@@ -4,18 +4,18 @@ import akka.actor.ActorRef
 import kz.mounty.fm.amqp.messages.AMQPMessage
 import kz.mounty.fm.amqp.messages.MountyMessages.{MountyApi, RoomCore, SpotifyGateway}
 import kz.mounty.fm.domain.commands._
+import kz.mounty.fm.domain.requests.{GetCurrentlyPlayingTrackGatewayRequestBody, GetCurrentlyPlayingTrackGatewayResponseBody, GetCurrentlyPlayingTrackRequestBody, GetCurrentlyPlayingTrackResponseBody}
 import kz.mounty.fm.domain.room.Room
-import kz.mounty.fm.domain.user.{RoomUser, RoomUserType}
-import kz.mounty.fm.exceptions.{ErrorCodes, ErrorSeries, ServerErrorRequestException}
+import kz.mounty.fm.domain.user.RoomUser
+import kz.mounty.fm.exceptions.{ErrorCodes, ErrorSeries, ExceptionInfo, ServerErrorRequestException}
 import org.json4s.Formats
 import org.json4s.jackson.Serialization._
-import org.json4s.native.JsonMethods._
 import org.mongodb.scala.MongoCollection
-import org.mongodb.scala.model.Filters.{and, equal}
+import org.mongodb.scala.model.Filters.equal
 import repositories.{RoomRepository, RoomUserRepository}
 import scredis.Redis
+import org.json4s.jackson.JsonMethods._
 
-import scala.collection.mutable
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Success}
 
@@ -59,7 +59,7 @@ class PlayerService(implicit val redis: Redis,
 
             case RoomCore.PlaySong.routingKey =>
               val body = parse(amqpMessage.entity).extract[PlaySongCommandBody]
-              val request = write(PlayerPlayGatewayCommandBody(deviceId = None, tokenKey = user.profileId, contextUri = body.contextUri, offset = body.offset))
+              val request = write(PlayerPlayGatewayCommandBody(deviceId = None, tokenKey = user.profileId, contextUri = body.contextUri, offset = body.offset, positionMs = body.positionMs))
               publisher ! AMQPMessage(request, "X:mounty-spotify-gateway-in", SpotifyGateway.PlayerPlayGatewayCommand.routingKey, amqpMessage.actorPath)
           }
         })
@@ -88,6 +88,28 @@ class PlayerService(implicit val redis: Redis,
         ).getMessage
         val reply = write(error)
         publisher ! amqpMessage.copy(entity = reply, routingKey = MountyApi.Error.routingKey, exchange = "X:mounty-api-out")
+    }
+  }
+
+  def getCurrentlyPlayingTrack(amqpMessage: AMQPMessage): Unit = {
+    val tokenKey = parse(amqpMessage.entity).extract[GetCurrentlyPlayingTrackRequestBody].tokenKey
+    publisher ! amqpMessage.copy(entity = write(GetCurrentlyPlayingTrackGatewayRequestBody(tokenKey)), routingKey = SpotifyGateway.GetCurrentlyPlayingTrackGatewayRequest.routingKey, exchange = "X:mounty-spotify-gateway-in")
+  }
+
+  def handleGetCurrentlyPlayingTrackGatewayResponse(amqpMessage: AMQPMessage): Unit = {
+    try {
+      val gatewayResponse = parse(amqpMessage.entity).extract[GetCurrentlyPlayingTrackGatewayResponseBody]
+      publisher ! amqpMessage.copy(entity = write(GetCurrentlyPlayingTrackResponseBody(gatewayResponse.track, gatewayResponse.progressMs)), routingKey = MountyApi.GetCurrentlyPlayingTrackResponse.routingKey, exchange = "X:mounty-api-out")
+    } catch {
+      case _: org.json4s.MappingException =>
+        val exceptionInfo = parse(amqpMessage.entity).extract[ExceptionInfo]
+        publisher ! amqpMessage.copy(entity = write(exceptionInfo), routingKey = MountyApi.Error.routingKey, exchange = "X:mounty-api-out")
+      case _ =>
+        val error = ServerErrorRequestException(
+          ErrorCodes.INTERNAL_SERVER_ERROR(ErrorSeries.ROOM_CORE),
+          Some("something went wrong while getting currently playing track")
+        ).getExceptionInfo
+        publisher ! amqpMessage.copy(entity = write(error), routingKey = MountyApi.Error.routingKey, exchange = "X:mounty-api-out")
     }
   }
 }
